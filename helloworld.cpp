3de4a2a0e82a663e5c7105726717ac7c00319a4c
hvaskjerikknoviler <Sverrejohansen@outlook.com>
2022-08-25 14:35:47 +0200

    git commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,15 @@
-// Look, our first C++ program!
 
 #include <iostream>
 #include <cmath>
 
-double x = 5;
-double main()
+double f(double x){
+    return 100*exp(-10*x);
+}
+int main()
 {
-  double f = 5*x;
-  return f;
+  double x = 0;
+  double res = f(x);
+  std::cout<<res <<"\n";
+  return 0;
 }
 
-main()
