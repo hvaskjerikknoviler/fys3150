6d0ac7959e28473aeeb26b29da55123a50809012
hvaskjerikknoviler <Sverrejohansen@outlook.com>
2022-08-25 15:21:42 +0200

    git commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,12 +3,18 @@
 #include <cmath>
 
 double f(double x){
-    return 100*exp(-10*x);
+  return 100*exp(-10*x);
 }
+double g(double x){
+  return exp(x);
+}
+
 int main()
 {
   double x = 0;
   double res = f(x);
+  double res2 = g(x);
+  std::cout<<res2 <<"\n";
   std::cout<<res <<"\n";
   return 0;
 }
